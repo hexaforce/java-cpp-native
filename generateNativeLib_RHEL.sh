8c57948a2735d6069d55ca0fa87e3970f4da0f65
Your Name <you@example.com>
2020-06-24 14:56:04 +0900

    update

--- generateNativeLib_RHEL.sh
+++ generateNativeLib_RHEL.sh
@@ -1,21 +1,27 @@
 #!/bin/bash -ex
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-cd $SCRIPT_DIR
 
-JDK_DIR=`readlink -f $(which java)`
-JDK_DIR=`dirname $JDK_DIR`
-JDK_DIR=`dirname $JDK_DIR`
-# Fedora
-JDK_DIR=`dirname $JDK_DIR`
+cd $SCRIPT_DIR/src/main/cpp
 
-cp -f $JDK_DIR/include/jni.h .
-cp -f $JDK_DIR/include/linux/jni_md.h .
+# yum install gcc-c++ java-1.8.0-openjdk-devel.x86_64
+JAVA_HOME=`readlink -f $(which java)`
+JAVA_HOME=`dirname $JAVA_HOME`
+JAVA_HOME=`dirname $JAVA_HOME`
+JAVA_HOME=`dirname $JAVA_HOME`
 
 # Create the header with javac -h . ClassName.java
 # Remember to set your JAVA_HOME env var
+
 g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux cpp_ExampleObjects.cpp -o cpp_ExampleObjects.o
 g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux cpp_ExampleParameters.cpp -o cpp_ExampleParameters.o
 g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux cpp_SayHello.cpp -o cpp_SayHello.o
-$SCRIPT_DIR/native/linux_x86_64
+
+mkdir -p $SCRIPT_DIR/native/linux_x86_64
+if [ -e ../../../native/linux_x86_64/libnative.so ]; then
+  rm ../../../native/linux_x86_64/libnative.so
+fi
+
 g++ -shared -fPIC -o ../../../native/linux_x86_64/libnative.so cpp_SayHello.o cpp_ExampleParameters.o cpp_ExampleObjects.o -lc
 # Don't forget to set java.library.path to point to the folder where you have the libnative you're loading.
+
+find . -name '*.o' | xargs rm 
