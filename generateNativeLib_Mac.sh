c54a82797aa69782d00f09f7c62e208b7b26cac3
Your Name <you@example.com>
2020-06-29 09:28:37 +0900

    aaa

--- generateNativeLib_Mac.sh
+++ generateNativeLib_Mac.sh
@@ -3,20 +3,28 @@ SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
 cd $SCRIPT_DIR/cpp
 
-JAVA_HOME=`/usr/libexec/java_home -v 8`
+JAVA_HOME=`/usr/libexec/java_home -v 1.8`
 
 # Create the header with javac -h . ClassName.java
 # Remember to set your JAVA_HOME env var
 
-g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin ExampleObjects.cpp -o ExampleObjects.o
-g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin ExampleParameters.cpp -o ExampleParameters.o
-g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin SayHello.cpp -o SayHello.o
+filelist=(
+    "ExampleObjects"
+    "ExampleParameters"
+    "SayHello"
+)
+
+compiled=''
+for file in "${filelist[@]}" ; do
+    g++ -c -fPIC -I${JAVA_HOME}/include -I${JAVA_HOME}/include/darwin ${file}.cpp -o ${file}.o
+    compiled=${compiled}' '${file}.o
+done
 
 mkdir -p $SCRIPT_DIR/native
 if [ -e $SCRIPT_DIR/native/libnative.dylib ]; then
   rm $SCRIPT_DIR/native/libnative.dylib
 fi
 
-g++ -dynamiclib -o $SCRIPT_DIR/native/libnative.dylib SayHello.o ExampleParameters.o ExampleObjects.o -lc
+g++ -dynamiclib -o $SCRIPT_DIR/native/libnative.dylib $compiled -lc
 
 find . -name '*.o' | xargs rm 
